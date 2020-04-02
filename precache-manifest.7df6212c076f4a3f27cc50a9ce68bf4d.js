f7e8baac1a69f7989062163c1c1a43acb60c73c3
halfacat1 <672047+halfacat1@users.noreply.github.com>
2020-04-02 22:55:59 +0000

    Deploying to gh-pages from  @ 68f760346c383c52a7f4e13b6cddfbc1a52ad654 🚀

---
+++ precache-manifest.7df6212c076f4a3f27cc50a9ce68bf4d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cfb39ebe675408fa99329968b29f3fa5",
+    "url": "/carta-historia/index.html"
+  },
+  {
+    "revision": "03e48fe02019d3c0ed8f",
+    "url": "/carta-historia/static/css/main.d5161ecd.chunk.css"
+  },
+  {
+    "revision": "8a3fd02097f18999ef0c",
+    "url": "/carta-historia/static/js/2.96be40cb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/carta-historia/static/js/2.96be40cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "03e48fe02019d3c0ed8f",
+    "url": "/carta-historia/static/js/main.b2fa11b3.chunk.js"
+  },
+  {
+    "revision": "a53fdda117b382bd185a",
+    "url": "/carta-historia/static/js/runtime-main.a19d14c3.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/carta-historia/precache-manifest.7df6212c076f4a3f27cc50a9ce68bf4d.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/carta-historia/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
