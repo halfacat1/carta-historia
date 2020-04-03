a2c336cb5119db0534469271a47d1d5ade8ec089
halfacat1 <672047+halfacat1@users.noreply.github.com>
2020-04-03 00:29:58 +0000

    Deploying to gh-pages from  @ f9ddbdf0a9e766b1aee471f328e1e025bf0753b3 🚀

---
+++ precache-manifest.1553b6f1b01bdd811527fb1c324533d5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84de7e2c8f3c93c8aa73ede511d8b5cb",
+    "url": "/carta-historia/index.html"
+  },
+  {
+    "revision": "05f7674e26b272c7ea05",
+    "url": "/carta-historia/static/css/main.d5161ecd.chunk.css"
+  },
+  {
+    "revision": "8a3fd02097f18999ef0c",
+    "url": "/carta-historia/static/js/2.96be40cb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/carta-historia/static/js/2.96be40cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "05f7674e26b272c7ea05",
+    "url": "/carta-historia/static/js/main.5224ae29.chunk.js"
+  },
+  {
+    "revision": "a53fdda117b382bd185a",
+    "url": "/carta-historia/static/js/runtime-main.a19d14c3.js"
+  }
+]);
